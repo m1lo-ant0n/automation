dcddcfc1ec2d78f2fb672215c1dbb02af40e50e7
m1lo-ant0n <139176496+m1lo-ant0n@users.noreply.github.com>
2023-08-19 18:20:29 -0300

    Delete src/main/java/com/icaro/clase5 directory

--- src/main/java/com/icaro/clase5/pack1/OtraClase.java
+++
@@ -1,9 +0,0 @@
-package com.icaro.clase5.pack1;
-
-public class OtraClase {
-
-    public void hacerCosas() {
-        Modificadores modificador = new Modificadores();
-//        modificador.variable2;
-    }
-}
